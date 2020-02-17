e7b0552e354f723acec5ad69ee5e9e85844bfe3a
Zizhou <jack.wang@flybits.com>
2020-02-17 15:02:24 -0500

    implement naughty button and demo

---
+++ nt-button.js
@@ -0,0 +1,83 @@
+class NaughtyButton extends HTMLElement {
+    constructor() {
+        super();
+        this._button;
+        this.attachShadow({ mode: 'open' });
+        this.shadowRoot.innerHTML = `
+        <style>
+            .nt-button {
+                min-width: 100px;
+                min-height: 32px;
+                padding: 4px 8px;
+                background-color: #1a53ff;
+                border: none;
+                border-radius: 16px;
+                color: #fff;
+                cursor: pointer;
+            }
+            .nt-button:hover, .nt-button:focus {
+                outline: none;
+                background-color: #0040ff;
+            }
+            .nt-button:active {
+                background-color: #0039e6;
+            }
+            .nt-button--disabled,
+            .nt-button--disabled:hover,
+            .nt-button--disabled:focus {
+                background-color: #e6e6e6;
+                cursor: no-drop;
+            }
+        </style>
+        <button class="nt-button"><slot><slot></button>
+        `
+    }
+    static get observedAttributes() {
+        return ['disabled', 'naughty'];
+    }
+    get disabled() {
+        return this.hasAttribute('disabled');
+    }
+    get naughty() {
+        return this.hasAttribute('naughty');
+    }
+    get onClick() {
+        if (this.hasAttribute('onclick') && !this.disabled) {
+            return eval(this.getAttribute('onclick'));
+        }
+        return () => undefined;
+    }
+    connectedCallback() {
+        this._button = this.shadowRoot.querySelector(".nt-button");
+        this._button.addEventListener('mouseenter', this._teleport.bind(this));
+        this._button.addEventListener('click', this.onClick.bind(this));
+    }
+    disconnectedCallback() {
+        this._button && this._button.removeEventListener('mouseenter', this._move);
+        this._button && this._button.removeEventListener('click', this.onClick);
+    }
+    attributeChangedCallback(name, oldValue, newValue) {
+        if (this.disabled) {
+          this.setAttribute('tabindex', '-1');
+          this.setAttribute('aria-disabled', 'true');
+          this.shadowRoot.querySelector(".nt-button").classList.add('nt-button--disabled')
+        } else {
+          this.setAttribute('tabindex', '0');
+          this.setAttribute('aria-disabled', 'false');
+        }
+    }
+    _teleport() {
+        if (this.naughty && !this.disabled) {
+            const button = this.shadowRoot.querySelector(".nt-button");
+            setTimeout(() => {
+                button.style.position = 'absolute';
+                button.style.left = `${Math.random() * (
+                    window.innerWidth - button.getBoundingClientRect().width)}px`;
+                button.style.top = `${Math.random() * (
+                    window.innerHeight - button.getBoundingClientRect().height)}px`;
+            }, 100);
+        }
+    }
+}
+
+customElements.define('nt-button', NaughtyButton);
